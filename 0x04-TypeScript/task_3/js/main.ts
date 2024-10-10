c768f8734ac59636b1adc1060d189e9698918770
MumbiDevs <113234080+MumbiDevs@users.noreply.github.com>
2024-10-10 05:38:19 +0300

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -4,12 +4,12 @@ import { RowID, RowElement } from "./interface";
 import * as CRUD from "./crud";
 
 const row: RowElement = {
-  firstName: "Suave",
-  lastName: "Maina",
+  firstName: "Guillaume",
+  lastName: "Salva",
 };
 
 const newRowID: RowID = CRUD.insertRow(row);
-const UpdatedRow: RowElement = { age: 34, ...row };
+const UpdatedRow: RowElement = { age: 23, ...row };
 
 CRUD.updateRow(newRowID, UpdatedRow);
 CRUD.deleteRow(newRowID);
